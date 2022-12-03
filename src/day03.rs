6c9a16e2351c019ff0b740c3f3072cc0ca118f8b
iTitus <iTitus@users.noreply.github.com>
2022-12-03 20:44:26 +0100

    use u8 for smaller footprint

--- src/day03.rs
+++ src/day03.rs
@@ -3,7 +3,7 @@ use std::collections::HashSet;
 use aoc_runner_derive::{aoc, aoc_generator};
 use itertools::Itertools;
 
-type Rucksack = (HashSet<u32>, HashSet<u32>);
+type Rucksack = (HashSet<u8>, HashSet<u8>);
 
 #[aoc_generator(day3)]
 pub fn input_generator(input: &str) -> Vec<Rucksack> {
@@ -16,14 +16,14 @@ pub fn input_generator(input: &str) -> Vec<Rucksack> {
         .collect()
 }
 
-fn letters_to_numbers(s: &str) -> HashSet<u32> {
+fn letters_to_numbers(s: &str) -> HashSet<u8> {
     s.chars().map(letter_to_number).collect()
 }
 
-fn letter_to_number(c: char) -> u32 {
+fn letter_to_number(c: char) -> u8 {
     match c {
-        'a'..='z' => (c as u32 - 'a' as u32) + 1,
-        'A'..='Z' => (c as u32 - 'A' as u32) + 27,
+        'a'..='z' => (c as u8 - b'a') + 1,
+        'A'..='Z' => (c as u8 - b'A') + 27,
         _ => panic!()
     }
 }
@@ -31,7 +31,7 @@ fn letter_to_number(c: char) -> u32 {
 #[aoc(day3, part1)]
 pub fn part1(input: &[Rucksack]) -> u32 {
     input.iter()
-        .map(|(l, r)| l.intersection(r).at_most_one().unwrap().unwrap())
+        .map(|(l, r)| *l.intersection(r).at_most_one().unwrap().unwrap() as u32)
         .sum()
 }
 
@@ -44,9 +44,9 @@ pub fn part2(input: &[Rucksack]) -> u32 {
             let mut rucksacks = g.into_iter()
                 .map(|(l, r)| l.union(r).collect::<HashSet<_>>());
             let first = rucksacks.next().unwrap();
-            rucksacks.fold(first, |a, e| &a & &e)
+            *rucksacks.fold(first, |a, e| &a & &e)
                 .into_iter()
-                .at_most_one().unwrap().unwrap()
+                .at_most_one().unwrap().unwrap() as u32
         })
         .sum()
 }
