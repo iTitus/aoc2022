2c04eba797fc9f80c855549385e81fbda1a3a819
iTitus <iTitus@users.noreply.github.com>
2022-12-06 11:31:36 +0100

    short but unoptimized version of day 06

---
+++ src/day06.rs
@@ -0,0 +1,23 @@
+use std::collections::HashSet;
+
+use aoc_runner_derive::aoc;
+use itertools::Itertools;
+
+fn find_first_distinct(input: &str, n: usize) -> usize {
+    // once again we assume that the input is only ascii
+    input.trim().as_bytes()
+        .windows(n)
+        .position(|window| window.iter().all_unique())
+        .map(|i| i + n)
+        .unwrap()
+}
+
+#[aoc(day6, part1)]
+pub fn part1(input: &str) -> usize {
+    find_first_distinct(input, 4)
+}
+
+#[aoc(day6, part2)]
+pub fn part2(input: &str) -> usize {
+    find_first_distinct(input, 14)
+}
