8fc6fed1b404b5f4ff0b0b54cd2e75ca3fb338cb
iTitus <iTitus@users.noreply.github.com>
2022-12-19 23:39:40 +0100

    day 19

--- src/day19.rs
+++ src/day19.rs
@@ -7,7 +7,7 @@ use regex::Regex;
 
 #[derive(Debug, Clone, Copy, Eq, PartialEq, Hash)]
 pub struct Blueprint {
-    index: u8,
+    index: u32,
     ore_robot_ore_cost: u32,
     clay_robot_ore_cost: u32,
     obsidian_robot_ore_cost: u32,
@@ -39,7 +39,7 @@ impl FromStr for Blueprint {
 
 #[derive(Debug, Clone, Copy, Eq, PartialEq, Hash)]
 struct State {
-    time_left: u8,
+    time_left: u32,
     ore_robot_count: u32,
     clay_robot_count: u32,
     obsidian_robot_count: u32,
@@ -51,7 +51,7 @@ struct State {
 }
 
 impl State {
-    fn initial(time_left: u8) -> State {
+    fn initial(time_left: u32) -> State {
         State {
             time_left,
             ore_robot_count: 1,
@@ -70,89 +70,109 @@ impl State {
             return;
         }
 
-        fn div_ceil<T: Integer>(a: T, b: T) -> T {
-            if a.is_zero() {
+        fn calc_wait_time<T: Integer>(wanted: T, existing: T, production: T) -> T {
+            if wanted <= existing {
                 T::zero()
             } else {
-                a.div_ceil(&b)
+                (wanted - existing).div_ceil(&production)
             }
         }
 
         if (self.obsidian_count >= blueprint.geode_robot_obsidian_cost || self.obsidian_robot_count > 0)
             && (self.ore_count >= blueprint.geode_robot_ore_cost || self.ore_robot_count > 0) {
-            let wait_time = div_ceil(blueprint.geode_robot_obsidian_cost - self.obsidian_count, self.obsidian_robot_count)
-                .max(div_ceil(blueprint.geode_robot_ore_cost - self.ore_count, self.ore_robot_count));
-            if wait_time < (self.time_left as u32) {
-
+            let wait_time = 1 + calc_wait_time(blueprint.geode_robot_obsidian_cost, self.obsidian_count, self.obsidian_robot_count)
+                .max(calc_wait_time(blueprint.geode_robot_ore_cost, self.ore_count, self.ore_robot_count));
+            if wait_time <= self.time_left {
+                q.push(State {
+                    time_left: self.time_left - wait_time,
+                    geode_robot_count: self.geode_robot_count + 1,
+                    ore_count: self.ore_count + wait_time * self.ore_robot_count - blueprint.geode_robot_ore_cost,
+                    clay_count: self.clay_count + wait_time * self.clay_robot_count,
+                    obsidian_count: self.obsidian_count + wait_time * self.obsidian_robot_count - blueprint.geode_robot_obsidian_cost,
+                    geode_count: self.geode_count + wait_time * self.geode_robot_count,
+                    ..*self
+                })
             }
         }
-        if self.time_left > 0 {
-            let ore_count = self.ore_count;
-            let clay_count = self.clay_count;
-            let obsidian_count = self.obsidian_count;
 
-            let _amounts = [0, 1];
-            let _amounts_rev = [1, 0];
-
-            //for geode_robot_production in &amounts_rev {
-            /*if ore_count < geode_robot_production * blueprint.geode_robot_ore_cost || obsidian_count < geode_robot_production * blueprint.geode_robot_obsidian_cost {
-                continue;
-            }*/
-            // always try to build a geode robot if possible
-            let geode_robot_production = u32::from(obsidian_count >= blueprint.geode_robot_obsidian_cost && ore_count >= blueprint.geode_robot_ore_cost);
-
-            let ore_count = ore_count - geode_robot_production * blueprint.geode_robot_ore_cost;
-            let obsidian_count = obsidian_count - geode_robot_production * blueprint.geode_robot_obsidian_cost;
-            for obsidian_robot_production in &_amounts {
-                if clay_count < obsidian_robot_production * blueprint.obsidian_robot_clay_cost || ore_count < obsidian_robot_production * blueprint.obsidian_robot_ore_cost {
-                    continue;
-                }
-
-                let ore_count = ore_count - obsidian_robot_production * blueprint.obsidian_robot_ore_cost;
-                let clay_count = clay_count - obsidian_robot_production * blueprint.obsidian_robot_clay_cost;
-                for clay_robot_production in &_amounts {
-                    if ore_count < clay_robot_production * blueprint.clay_robot_ore_cost {
-                        continue;
-                    }
-
-                    let ore_count = ore_count - clay_robot_production * blueprint.clay_robot_ore_cost;
-                    for ore_robot_production in &_amounts {
-                        if ore_count < ore_robot_production * blueprint.ore_robot_ore_cost {
-                            continue;
-                        }
-
-                        let ore_count = ore_count - ore_robot_production * blueprint.ore_robot_ore_cost;
-                        let mut s = *self;
-                        s.time_left -= 1;
-                        s.ore_robot_count += ore_robot_production;
-                        s.clay_robot_count += clay_robot_production;
-                        s.obsidian_robot_count += obsidian_robot_production;
-                        s.geode_robot_count += geode_robot_production;
-                        s.ore_count = ore_count + self.ore_robot_count;
-                        s.clay_count = clay_count + self.clay_robot_count;
-                        s.obsidian_count = obsidian_count + self.obsidian_robot_count;
-                        s.geode_count += self.geode_robot_count;
-                        // println!("  succ: {s:?}");
-                        q.push(s);
-                    }
-                }
+        if (self.clay_count >= blueprint.obsidian_robot_clay_cost || self.clay_robot_count > 0)
+            && (self.ore_count >= blueprint.obsidian_robot_ore_cost || self.ore_robot_count > 0) {
+            let wait_time = 1 + calc_wait_time(blueprint.obsidian_robot_clay_cost, self.clay_count, self.clay_robot_count)
+                .max(calc_wait_time(blueprint.obsidian_robot_ore_cost, self.ore_count, self.ore_robot_count));
+            if wait_time <= self.time_left {
+                q.push(State {
+                    time_left: self.time_left - wait_time,
+                    obsidian_robot_count: self.obsidian_robot_count + 1,
+                    ore_count: self.ore_count + wait_time * self.ore_robot_count - blueprint.obsidian_robot_ore_cost,
+                    clay_count: self.clay_count + wait_time * self.clay_robot_count - blueprint.obsidian_robot_clay_cost,
+                    obsidian_count: self.obsidian_count + wait_time * self.obsidian_robot_count,
+                    geode_count: self.geode_count + wait_time * self.geode_robot_count,
+                    ..*self
+                })
             }
-            //}
+        }
+
+        if self.ore_count >= blueprint.clay_robot_ore_cost || self.ore_robot_count > 0 {
+            let wait_time = 1 + calc_wait_time(blueprint.clay_robot_ore_cost, self.ore_count, self.ore_robot_count);
+            if wait_time <= self.time_left {
+                q.push(State {
+                    time_left: self.time_left - wait_time,
+                    clay_robot_count: self.clay_robot_count + 1,
+                    ore_count: self.ore_count + wait_time * self.ore_robot_count - blueprint.clay_robot_ore_cost,
+                    clay_count: self.clay_count + wait_time * self.clay_robot_count,
+                    obsidian_count: self.obsidian_count + wait_time * self.obsidian_robot_count,
+                    geode_count: self.geode_count + wait_time * self.geode_robot_count,
+                    ..*self
+                })
+            }
+        }
+
+        if self.ore_count >= blueprint.ore_robot_ore_cost || self.ore_robot_count > 0 {
+            let wait_time = 1 + calc_wait_time(blueprint.ore_robot_ore_cost, self.ore_count, self.ore_robot_count);
+            if wait_time <= self.time_left {
+                q.push(State {
+                    time_left: self.time_left - wait_time,
+                    ore_robot_count: self.ore_robot_count + 1,
+                    ore_count: self.ore_count + wait_time * self.ore_robot_count - blueprint.ore_robot_ore_cost,
+                    clay_count: self.clay_count + wait_time * self.clay_robot_count,
+                    obsidian_count: self.obsidian_count + wait_time * self.obsidian_robot_count,
+                    geode_count: self.geode_count + wait_time * self.geode_robot_count,
+                    ..*self
+                })
+            }
         }
     }
 
-    fn max_geode_count_heuristic(&self) -> u32 {
-        let production = self.geode_count + self.geode_robot_count * (self.time_left as u32);
+    fn geode_count_upper_bound(&self) -> u32 {
+        let production = self.geode_count + self.geode_robot_count * self.time_left;
         if self.time_left == 0 {
             production
         } else {
-            let time_left = self.time_left as u32;
             production + {
-                let time_left_m1 = time_left - 1;
+                let time_left_m1 = self.time_left - 1;
                 (time_left_m1 * time_left_m1 + time_left_m1) / 2
             }
         }
     }
+}
+
+fn max_geode_count(time_left: u32, blueprint: &Blueprint) -> u32 {
+    let mut max_geode_count = 0;
+    let mut q = vec![];
+    q.push(State::initial(time_left));
+    while let Some(s) = q.pop() {
+        if s.geode_count > max_geode_count {
+            max_geode_count = s.geode_count;
+        }
+
+        if s.geode_count_upper_bound() <= max_geode_count {
+            continue;
+        }
+
+        s.add_successors(blueprint, &mut q);
+    }
+
+    max_geode_count
 }
 
 #[aoc_generator(day19)]
@@ -167,39 +187,16 @@ pub fn input_generator(input: &str) -> Vec<Blueprint> {
 #[aoc(day19, part1)]
 pub fn part1(input: &[Blueprint]) -> u32 {
     input.iter()
-        .map(|blueprint| {
-            println!("### BP {} ###", blueprint.index);
-
-            let mut max_geode_count = 0;
-            // let mut visited = FxHashSet::default();
-            let mut q = vec![];
-            q.push(State::initial(23));
-            while let Some(s) = q.pop() {
-                /*if !visited.insert(s) {
-                    continue;
-                }*/
-
-                // println!("{s:?}");
-                if (s.geode_count as u32) > max_geode_count {
-                    println!("new max geode count: {s:?}");
-                    max_geode_count = s.geode_count as u32;
-                }
-
-                if s.max_geode_count_heuristic() <= max_geode_count {
-                    continue;
-                }
-
-                s.add_successors(blueprint, &mut q);
-            }
-
-            (blueprint.index as u32) * max_geode_count
-        })
+        .map(|blueprint| (blueprint.index as u32) * max_geode_count(24, blueprint))
         .sum()
 }
 
 #[aoc(day19, part2)]
-pub fn part2(_: &[Blueprint]) -> usize {
-    0
+pub fn part2(input: &[Blueprint]) -> u32 {
+    input.iter()
+        .take(3)
+        .map(|blueprint| max_geode_count(32, blueprint))
+        .product()
 }
 
 
@@ -215,13 +212,5 @@ mod tests {
 Blueprint 2: Each ore robot costs 2 ore. Each clay robot costs 3 ore. Each obsidian robot costs 3 ore and 8 clay. Each geode robot costs 3 ore and 12 obsidian.
 ");
         assert_eq!(33, part1(&input))
-    }
-
-    #[test]
-    fn test_2() {
-        let input = input_generator(r"Blueprint 1: Each ore robot costs 4 ore. Each clay robot costs 2 ore. Each obsidian robot costs 3 ore and 14 clay. Each geode robot costs 2 ore and 7 obsidian.
-Blueprint 2: Each ore robot costs 2 ore. Each clay robot costs 3 ore. Each obsidian robot costs 3 ore and 8 clay. Each geode robot costs 3 ore and 12 obsidian.
-");
-        assert_eq!(0, part2(&input))
     }
 }
